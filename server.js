1e11a912b3cc33d934194943638e6636719b34a1
PerNorrgren <per.norrgren@outlook.com>
2026-06-27 20:10:32 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -408,7 +408,7 @@ async function textToSpeech(text) {
       headers: { 'xi-api-key': ELEVENLABS_API_KEY, 'Content-Type': 'application/json' },
       body: JSON.stringify({
         text,
-        model_id: 'eleven_turbo_v2',
+        model_id: 'eleven_multilingual_v2',
         voice_settings: { stability: 0.78, similarity_boost: 0.85, style: 0.0, use_speaker_boost: true },
         speed: VOICE_SPEED,
       }),
